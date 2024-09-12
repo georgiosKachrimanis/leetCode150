10eebddab957d91b4128fe36ad9a4b1fb8dc221b
Georgios Kachrimanis <georgios.kachrimanis@hotmail.com>
2024-09-12 14:57:49 +0200

    updated readme.md

--- ArrayString/indexFirstOccurrence.cpp
+++ ArrayString/indexFirstOccurrence.cpp
@@ -12,16 +12,20 @@ public:
         int haystackSize = haystack.size();
         int needleSize = needle.size();
 
-         for (int i = 0; i <= haystackSize - needleSize; i++) {
+        for (int i = 0; i <= haystackSize - needleSize; i++)
+        {
             // Check if the substring in haystack matches the needle
             int j;
-            for (j = 0; j < needleSize; j++) {
-                if (haystack[i + j] != needle[j]) {
-                    break;  // Mismatch found, break the inner loop
+            for (j = 0; j < needleSize; j++)
+            {
+                if (haystack[i + j] != needle[j])
+                {
+                    break; // Mismatch found, break the inner loop
                 }
             }
             // If the entire needle was matched, return the starting index
-            if (j == needleSize) {
+            if (j == needleSize)
+            {
                 return i;
             }
         }
