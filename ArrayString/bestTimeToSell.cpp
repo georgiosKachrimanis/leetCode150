d2cb1f80199df818cd99eee25de82fce06c2c0bd
Georgios Kachrimanis <georgios.kachrimanis@hotmail.com>
2024-09-19 10:07:24 +0200

    Added solution for Best time to buy and sell I

---
+++ ArrayString/bestTimeToSell.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        
+
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+            else if(prices[i]- minPrice > maxProfit)
+            {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+};
--- TwoPointers/twoSumII.cpp
+++ TwoPointers/twoSumII.cpp
@@ -5,24 +5,27 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         unordered_map<int, int> numbersMap;
-        // First create an unordered map with the values of nums 
-        for (int i = 0; i < nums.size(); i++){
+        // First create an unordered map with the values of nums
+        for (int i = 0; i < nums.size(); i++)
+        {
             numbersMap[nums[i]] = i;
         }
 
-        // Now for each number in the nums vector, I will check if there is a number inside the numbersMap 
-        // that is equal to the difference of target - current value of nums iteration. If there is such a number(difference) and is not the same number as the current iteration of nums 
+        // Now for each number in the nums vector, I will check if there is a number inside the numbersMap
+        // that is equal to the difference of target - current value of nums iteration. If there is such a number(difference) and is not the same number as the current iteration of nums
         // then I return the position of the curent iteration and the position of the difference.
         for (int i = 0; i < nums.size(); i++)
         {
             int difference = target - nums[i];
             if (numbersMap.count(difference) && numbersMap[difference] != i)
             {
-                return {i+1, numbersMap[difference]+1};
+                return {i + 1, numbersMap[difference] + 1};
             }
         }
         return {};
